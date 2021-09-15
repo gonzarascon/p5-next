47dd945cec265545363829a0befa5655c2da9a1b
Gonzalo Rascón <gonzarascon@gmail.com>
2021-09-15 04:04:58 +0000

    Changes to be committed: 	modified:   p5/base.p5.ts 	modified:   package.json 	modified:   pages/index.tsx 	modified:   tailwind.config.js 	modified:   yarn.lock

--- p5/base.p5.ts
+++ p5/base.p5.ts
@@ -1,16 +1,31 @@
 import type p5Type from 'p5'
 
 let x = 50
-const y = 50
+let y = 50
+
+const canvasX = 500
+const canvasY = 500
 
 export const setup = (p5: p5Type, canvasParentRef: Element): void => {
-  p5.createCanvas(500, 500).parent(canvasParentRef)
+  p5.createCanvas(canvasX, canvasY).parent(canvasParentRef)
 }
 
 export const draw = (p5: p5Type): void => {
   p5.background(0)
   p5.ellipse(x, y, 70, 70)
-  if (x < 500) {
+  if (x !== canvasX - 50 && y === 50) {
     x++
+  }
+
+  if (x === canvasX - 50 && y !== canvasY - 50) {
+    y++
+  }
+
+  if (y === canvasY - 50 && x !== 50) {
+    x--
+  }
+
+  if (y !== 50 && x === 50) {
+    y--
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,43 @@
 import Head from 'next/head'
+import dynamic from 'next/dynamic'
 import { draw, setup } from 'p5/base.p5'
-import Sketch from 'react-p5'
+
+const Sketch = dynamic(import('react-p5'), { ssr: false })
 
 export const Home = (): JSX.Element => (
   <>
     <Head>
-      <title>Create Next App</title>
+      <title>Wikipedia super merger</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
-    <h2 className="text-2xl">
-      Welcome to <a href="https://nextjs.org">Next.js!</a>
-    </h2>
+    <div className="container mx-auto">
+      <h2 className="text-2xl font-bold mx-auto text-center mb-10">
+        Wiki-photo super merger
+      </h2>
 
-    <div className="flex items-center justify-center">
-      <Sketch setup={setup} draw={draw} />
+      <section className="flex flex-nowrap mx-auto space-x-4 justify-center py-10">
+        <div>
+          <span className="block mb-2">First article</span>
+          <input
+            placeholder="Search an article"
+            type="text"
+            className="rounded-md border-gray-300 shadow-sm focus:ring focus:outline-none focus:border-indigo-500 focus:ring-indigo-500 focus:ring-opacity-20"
+          />
+        </div>
+        <div>
+          <span className="block mb-2">Second article</span>
+          <input
+            placeholder="Search an article"
+            type="text"
+            className="rounded-md border-gray-300 shadow-sm focus:ring focus:outline-none focus:border-pink-500 focus:ring-pink-500 focus:ring-opacity-20"
+          />
+        </div>
+      </section>
+
+      <section className="flex items-center justify-center">
+        <Sketch setup={setup} draw={draw} />
+      </section>
     </div>
   </>
 )
